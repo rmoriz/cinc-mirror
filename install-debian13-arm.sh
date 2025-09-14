94aafbb38e23ab1504086d4c0791ae0222c66770
Roland Moriz <roland@moriz.de>
2025-09-14 07:40:18 +0200

    fix: deb filename generation adopts upstream and maps arch (aarch64->arm64, x86_64->amd64); fallback version populated from metadata; update GHCR repo path

--- install-debian13-arm.sh
+++ install-debian13-arm.sh
@@ -411,123 +411,12 @@ fi
 # endpoint out there).
 #
 
-machine=`uname -m`
-os=`uname -s`
+# Hardcoded platform detection for Debian 13 ARM64
+platform="debian"
+platform_version="13"
+machine="aarch64"
 
-if test -f "/etc/lsb-release" && grep DISTRIB_ID /etc/lsb-release >/dev/null && ! grep wrlinux /etc/lsb-release >/dev/null; then
-  platform=`grep DISTRIB_ID /etc/lsb-release | cut -d "=" -f 2 | tr '[A-Z]' '[a-z]'`
-  platform_version=`grep DISTRIB_RELEASE /etc/lsb-release | cut -d "=" -f 2`
-
-  if test "$platform" = "\"cumulus linux\""; then
-    platform="cumulus_linux"
-  elif test "$platform" = "\"cumulus networks\""; then
-    platform="cumulus_networks"
-  fi
-
-elif test -f "/etc/debian_version"; then
-  platform="debian"
-  platform_version=`cat /etc/debian_version`
-elif test -f "/etc/Eos-release"; then
-  # EOS may also contain /etc/redhat-release so this check must come first.
-  platform=arista_eos
-  platform_version=`awk '{print $4}' /etc/Eos-release`
-  machine="i386"
-elif test -f "/etc/redhat-release"; then
-  platform=`sed 's/^\(.\+\) release.*/\1/' /etc/redhat-release | tr '[A-Z]' '[a-z]'`
-  platform_version=`sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release`
-  
-  if test "$platform" = "rocky linux"; then
-  	source /etc/os-release
- 	os="${REDHAT_SUPPORT_PRODUCT}"
-  	platform_version="${ROCKY_SUPPORT_PRODUCT_VERSION}"
-        platform=$ID
-
-  elif test "$platform" = "xenserver"; then
-    # Current XenServer 6.2 is based on CentOS 5, platform is not reset to "el" server should handle response
-    platform="xenserver"
-  else
-    # FIXME: use "redhat"
-    platform="el"
-  fi
-
-elif test -f "/etc/system-release"; then
-  platform=`sed 's/^\(.\+\) release.\+/\1/' /etc/system-release | tr '[A-Z]' '[a-z]'`
-  platform_version=`sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/system-release | tr '[A-Z]' '[a-z]'`
-  case $platform in amazon*) # sh compat method of checking for a substring
-    . /etc/os-release
-    platform_version=$VERSION_ID
-
-    case $platform_version in
-      "2022"|"2023")
-        platform="amazon"
-        platform_version=$platform_version
-      ;;
-      "2")
-        platform="el"
-        platform_version="7"
-        ;;
-      *)
-        platform="el"
-
-        # VERSION_ID will match YYYY.MM for Amazon Linux AMIs
-        platform_version="6"
-        ;;
-    esac
-  esac
-
-
-# Apple macOS
-elif test -f "/usr/bin/sw_vers"; then
-  platform="mac_os_x"
-  # Matching the tab-space with sed is error-prone
-  platform_version=`sw_vers | awk '/^ProductVersion:/ { print $2 }' | cut -d. -f1,2`
-elif test -f "/etc/release"; then
-  machine=`/usr/bin/uname -p`
-  if grep SmartOS /etc/release >/dev/null; then
-    platform="smartos"
-    platform_version=`grep ^Image /etc/product | awk '{ print $3 }'`
-  else
-    platform="solaris2"
-    platform_version=`/usr/bin/uname -r`
-  fi
-elif test -f "/etc/SuSE-release"; then
-  if grep 'Enterprise' /etc/SuSE-release >/dev/null;
-  then
-      platform="sles"
-      platform_version=`awk '/^VERSION/ {V = $3}; /^PATCHLEVEL/ {P = $3}; END {print V "." P}' /etc/SuSE-release`
-  else # opensuse 43 only. 15 ships with /etc/os-release only
-      platform="opensuseleap"
-      platform_version=`awk '/^VERSION =/ { print $3 }' /etc/SuSE-release`
-  fi
-elif test "x$os" = "xFreeBSD"; then
-  platform="freebsd"
-  platform_version=`uname -r | sed 's/-.*//'`
-elif test "x$os" = "xAIX"; then
-  platform="aix"
-  platform_version="`uname -v`.`uname -r`"
-  machine="powerpc"
-elif test -f "/etc/os-release"; then
-  . /etc/os-release
-  if test "x$CISCO_RELEASE_INFO" != "x"; then
-    . $CISCO_RELEASE_INFO
-  fi
-
-  platform=$ID
-
-  # VERSION_ID is always the preferred variable to use, but not
-  # every distro has it so fallback to VERSION
-  if test "x$VERSION_ID" != "x"; then
-    platform_version=$VERSION_ID
-  else
-    platform_version=$VERSION
-  fi
-fi
-
-if test "x$platform" = "x"; then
-  echo "Unable to determine platform version!"
-  report_bug
-  exit 1
-fi
+echo "$platform $platform_version $machine"
 
 #
 # NOTE: platform mangling in the install.sh is DEPRECATED
@@ -762,9 +651,8 @@ else
       aarch64) deb_arch="arm64" ;;
       x86_64)  deb_arch="amd64" ;;
     esac
-    # Fallback synthesized filename (will be overwritten if we can adopt upstream name)
     filename="${project}_${version}_${platform}_${platform_version}_${deb_arch}.${filetype}"
-    # If upstream metadata (packages.cinc.sh) provides an original filename (includes revision like -1), adopt it
+    # If metadata URL provides original filename (including revision like -1), adopt it
     original_filename=`basename "$download_url"`
     case "$original_filename" in
       ${project}_*_${deb_arch}.deb)
