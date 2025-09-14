cd2fa96ca6135a2589c630b7f7835de621398b8a
Roland Moriz <roland@moriz.de>
2025-09-14 03:06:25 +0200

    Add missing FTP functions to mirror script
    - Add get_versions() function to fetch available versions from FTP
    - Add get_distros() function to get available distributions
    - Add get_distro_versions() function to get distro versions
    - Fix the undefined function errors that were causing workflow failures

--- mirror-cinc.sh
+++ mirror-cinc.sh
@@ -44,6 +44,39 @@ log_error() {
     echo -e "${RED}[ERROR]${NC} $1"
 }
 
+# Get list of versions from FTP
+get_versions() {
+    log_info "Fetching available versions from FTP..."
+    local versions
+    versions=$(curl -s -l "$FTP_BASE/" | sort -V)
+
+    # Filter versions >= MIN_VERSION
+    local filtered_versions=()
+    for version in $versions; do
+        if [[ $version =~ ^([0-9]+)\. ]]; then
+            local major_version="${BASH_REMATCH[1]}"
+            if (( major_version >= MIN_VERSION )); then
+                filtered_versions+=("$version")
+            fi
+        fi
+    done
+
+    echo "${filtered_versions[@]}"
+}
+
+# Get distros for a version
+get_distros() {
+    local version="$1"
+    curl -s -l "$FTP_BASE/$version/" | grep -E "^($(IFS=\|; echo "${TARGET_DISTROS_ARRAY[*]}"))$"
+}
+
+# Get distro versions for a distro
+get_distro_versions() {
+    local version="$1"
+    local distro="$2"
+    curl -s -l "$FTP_BASE/$version/$distro/"
+}
+
 # Check dependencies
 check_dependencies() {
     local missing_deps=()
